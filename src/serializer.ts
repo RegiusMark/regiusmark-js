0b7cbb1c2e08edb937c9295f3dae93ef04f8bf6c
samrg472 <samrg472@gmail.com>
2019-08-06 12:25:41 -0700

    Initial (de)serialization impl of txs

---
+++ src/serializer.ts
@@ -0,0 +1,86 @@
+import { PublicKey, SigPair, ScriptHash } from './crypto';
+import ByteBuffer from 'bytebuffer';
+import { sign } from 'tweetnacl';
+import { Asset } from './asset';
+import Long from 'long';
+
+export class TypeSerializer {
+  public static buffer(buf: ByteBuffer, value: Uint8Array): void {
+    let len = value.length;
+    buf.writeUint32(len);
+    buf.append(value);
+  }
+
+  public static sizedBuffer(buf: ByteBuffer, value: Uint8Array): void {
+    buf.writeBytes(value);
+  }
+
+  public static string(buf: ByteBuffer, value: string): void {
+    buf.writeUTF8String(value);
+  }
+
+  public static publicKey(buf: ByteBuffer, value: PublicKey): void {
+    TypeSerializer.sizedBuffer(buf, value.buffer);
+  }
+
+  public static scriptHash(buf: ByteBuffer, value: ScriptHash): void {
+    TypeSerializer.sizedBuffer(buf, value);
+  }
+
+  public static sigPair(buf: ByteBuffer, value: SigPair): void {
+    TypeSerializer.publicKey(buf, value.publicKey);
+    TypeSerializer.sizedBuffer(buf, value.signature);
+  }
+
+  public static asset(buf: ByteBuffer, value: Asset): void {
+    let num = Long.fromString(value.amount.toFixed(0), false);
+    buf.writeVarint64ZigZag(num);
+  }
+}
+
+export class TypeDeserializer {
+  public static buffer(buf: ByteBuffer): Uint8Array {
+    const len = buf.readUint32();
+    if (len === 0) return new Uint8Array();
+    return TypeDeserializer.sizedBuffer(buf, len);
+  }
+
+  public static sizedBuffer(buf: ByteBuffer, len: number): Uint8Array {
+    return new Uint8Array(buf.readBytes(len).toArrayBuffer());
+  }
+
+  public static string(buf: ByteBuffer): string {
+    const len = buf.readUint32();
+    if (len === 0) return '';
+    let res = buf.readUTF8String(len);
+    if (typeof res === 'string') {
+      return res;
+    } else {
+      return res.string;
+    }
+  }
+
+  public static publicKey(buf: ByteBuffer): PublicKey {
+    const wif = TypeDeserializer.sizedBuffer(buf, sign.publicKeyLength);
+    return new PublicKey(wif);
+  }
+
+  public static scriptHash(buf: ByteBuffer): ScriptHash {
+    // Sha256 Hash is 32-bytes
+    return TypeDeserializer.sizedBuffer(buf, 32);
+  }
+
+  public static sigPair(buf: ByteBuffer): SigPair {
+    const publicKey = TypeDeserializer.publicKey(buf);
+    const signature = TypeDeserializer.sizedBuffer(buf, sign.signatureLength);
+    return {
+      publicKey,
+      signature,
+    };
+  }
+
+  public static asset(buf: ByteBuffer): Asset {
+    const amt = Big(buf.readVarint64ZigZag().toString());
+    return new Asset(amt);
+  }
+}
--- src/tx.ts
+++ src/tx.ts
@@ -1,4 +1,5 @@
 import { PublicKey, SigPair, ScriptHash, Script } from './crypto';
+import { TypeSerializer, TypeDeserializer } from './serializer';
 import { Asset } from './asset';
 import Long from 'long';
 
@@ -9,23 +10,108 @@ export enum TxType {
   TRANSFER = 3,
 }
 
+export type TxVariant = OwnerTx | MintTx | RewardTx | TransferTx;
+
 export interface TxData {
   timestamp: Long; // unsigned 64-bit integer, epoch time in ms
   fee: Asset;
   signaturePairs: SigPair[];
 }
 
-export class Tx {
+export abstract class Tx {
   public readonly type: TxType;
   public timestamp: Long;
   public fee: Asset;
   public signaturePairs: SigPair[];
 
   public constructor(type: TxType, data: TxData) {
+    if (!data.timestamp.unsigned) {
+      throw new Error('timestamp must be unsigned long');
+    }
     this.type = type;
     this.timestamp = data.timestamp;
     this.fee = data.fee;
     this.signaturePairs = data.signaturePairs;
+  }
+
+  public abstract serializeData(buf: ByteBuffer): void;
+
+  public serialize(buf: ByteBuffer, includeSigs: boolean = true): void {
+    this.serializeHeader(buf);
+    this.serializeData(buf);
+
+    if (includeSigs) {
+      buf.writeUint8(this.signaturePairs.length);
+      for (const sig of this.signaturePairs) {
+        TypeSerializer.sigPair(buf, sig);
+      }
+    }
+  }
+
+  public serializeHeader(buf: ByteBuffer): void {
+    buf.writeUint8(this.type);
+    buf.writeUint64(this.timestamp);
+    TypeSerializer.asset(buf, this.fee);
+  }
+
+  public static deserialize(buf: ByteBuffer): TxVariant {
+    const header = Tx.deserializeHeader(buf);
+
+    switch (header[0]) {
+      case TxType.OWNER: {
+        const data = OwnerTx.deserializeData(buf);
+        header[1].signaturePairs = Tx.deserializeSigs(buf);
+        return new OwnerTx(header[1], data);
+      }
+      case TxType.MINT: {
+        const data = MintTx.deserializeData(buf);
+        header[1].signaturePairs = Tx.deserializeSigs(buf);
+        return new MintTx(header[1], data);
+      }
+      case TxType.REWARD: {
+        const data = RewardTx.deserializeData(buf);
+        header[1].signaturePairs = Tx.deserializeSigs(buf);
+        return new RewardTx(header[1], data);
+      }
+      case TxType.TRANSFER: {
+        const data = TransferTx.deserializeData(buf);
+        header[1].signaturePairs = Tx.deserializeSigs(buf);
+        return new TransferTx(header[1], data);
+      }
+      default:
+        throw new Error('unknown tx type deserializing tx: ' + header[0]);
+    }
+  }
+
+  public static deserializeHeader(buf: ByteBuffer): [TxType, TxData] {
+    const type = buf.readUint8() as TxType;
+    switch (type) {
+      case TxType.OWNER:
+        break;
+      case TxType.MINT:
+        break;
+      case TxType.REWARD:
+        break;
+      case TxType.TRANSFER:
+        break;
+      default:
+        throw new Error('unknown tx type deserializing header: ' + type);
+    }
+
+    const timestamp = buf.readUint64();
+    const fee = TypeDeserializer.asset(buf);
+    const signaturePairs: SigPair[] = [];
+
+    return [type, { timestamp, fee, signaturePairs }];
+  }
+
+  private static deserializeSigs(buf: ByteBuffer): SigPair[] {
+    const sigLen = buf.readUint8();
+    const sigs = [];
+    for (let i = 0; i < sigLen; ++i) {
+      sigs.push(TypeDeserializer.sigPair(buf));
+    }
+    return sigs;
   }
 }
 
@@ -37,14 +123,32 @@ export interface OwnerTxData {
 
 export class OwnerTx extends Tx implements OwnerTxData {
   public minter: PublicKey;
-  public wallet: Uint8Array;
-  public script: Uint8Array;
+  public wallet: ScriptHash;
+  public script: Script;
 
   public constructor(base: TxData, data: OwnerTxData) {
     super(TxType.OWNER, base);
     this.minter = data.minter;
     this.wallet = data.wallet;
     this.script = data.script;
+  }
+
+  public serializeData(buf: ByteBuffer): void {
+    TypeSerializer.publicKey(buf, this.minter);
+    TypeSerializer.scriptHash(buf, this.wallet);
+    TypeSerializer.buffer(buf, this.script);
+  }
+
+  public static deserializeData(buf: ByteBuffer): OwnerTxData {
+    const minter = TypeDeserializer.publicKey(buf);
+    const wallet = TypeDeserializer.scriptHash(buf);
+    const script = TypeDeserializer.buffer(buf);
+
+    return {
+      minter,
+      wallet,
+      script,
+    };
   }
 }
 
@@ -71,6 +175,30 @@ export class MintTx extends Tx implements MintTxData {
     this.attachmentName = data.attachmentName;
     this.script = data.script;
   }
+
+  public serializeData(buf: ByteBuffer): void {
+    TypeSerializer.scriptHash(buf, this.to);
+    TypeSerializer.asset(buf, this.amount);
+    TypeSerializer.buffer(buf, this.attachment);
+    TypeSerializer.string(buf, this.attachmentName);
+    TypeSerializer.buffer(buf, this.script);
+  }
+
+  public static deserializeData(buf: ByteBuffer): MintTxData {
+    const to = TypeDeserializer.scriptHash(buf);
+    const amount = TypeDeserializer.asset(buf);
+    const attachment = TypeDeserializer.buffer(buf);
+    const attachmentName = TypeDeserializer.string(buf);
+    const script = TypeDeserializer.buffer(buf);
+
+    return {
+      to,
+      amount,
+      attachment,
+      attachmentName,
+      script,
+    };
+  }
 }
 
 export interface RewardTxData {
@@ -86,6 +214,21 @@ export class RewardTx extends Tx implements RewardTxData {
     super(TxType.REWARD, base);
     this.to = data.to;
     this.rewards = data.rewards;
+  }
+
+  public serializeData(buf: ByteBuffer): void {
+    TypeSerializer.scriptHash(buf, this.to);
+    TypeSerializer.asset(buf, this.rewards);
+  }
+
+  public static deserializeData(buf: ByteBuffer): RewardTxData {
+    const to = TypeDeserializer.scriptHash(buf);
+    const rewards = TypeDeserializer.asset(buf);
+
+    return {
+      to,
+      rewards,
+    };
   }
 }
 
@@ -111,5 +254,29 @@ export class TransferTx extends Tx implements TransferTxData {
     this.script = data.script;
     this.amount = data.amount;
     this.memo = data.memo;
+  }
+
+  public serializeData(buf: ByteBuffer): void {
+    TypeSerializer.scriptHash(buf, this.from);
+    TypeSerializer.scriptHash(buf, this.to);
+    TypeSerializer.buffer(buf, this.script);
+    TypeSerializer.asset(buf, this.amount);
+    TypeSerializer.buffer(buf, this.memo);
+  }
+
+  public static deserializeData(buf: ByteBuffer): TransferTxData {
+    const from = TypeDeserializer.scriptHash(buf);
+    const to = TypeDeserializer.scriptHash(buf);
+    const script = TypeDeserializer.buffer(buf);
+    const amount = TypeDeserializer.asset(buf);
+    const memo = TypeDeserializer.buffer(buf);
+
+    return {
+      from,
+      to,
+      script,
+      amount,
+      memo,
+    };
   }
 }
